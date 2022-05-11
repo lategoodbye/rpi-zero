1832a7b28a3788868d744174bcc75a39a32df767
Stefan Wahren <stefan.wahren@i2se.com>
2022-05-11 22:33:43 +0200

    mfd: bcm2835-pm: fix up
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

--- drivers/mfd/bcm2835-pm.c
+++ drivers/mfd/bcm2835-pm.c
@@ -17,9 +17,6 @@
 #include <linux/types.h>
 #include <linux/watchdog.h>
 
-#define BCM2835	1
-#define BCM2711	2
-
 static const struct mfd_cell bcm2835_pm_devs[] = {
 	{ .name = "bcm2835-wdt" },
 };
@@ -31,7 +28,7 @@ static const struct mfd_cell bcm2835_power_devs[] = {
 static int bcm2835_pm_get_pdata(struct platform_device *pdev,
 				struct bcm2835_pm *pm)
 {
-	bool is_bcm2711 = (uintptr_t)device_get_match_data(pm->dev) == BCM2711;
+	bool is_bcm2711 = of_device_is_compatible(pm->dev->of_node, "brcm,bcm2711-pm");
 
 	/* If no 'reg-names' property is found we can assume we're using old
 	 * firmware.
@@ -44,30 +41,34 @@ static int bcm2835_pm_get_pdata(struct platform_device *pdev,
 			return PTR_ERR(pm->base);
 
 		pm->asb = devm_platform_ioremap_resource(pdev, 1);
-		if (IS_ERR(pm->asb))
-			pm->asb = NULL;
-
 		pm->rpivid_asb = devm_platform_ioremap_resource(pdev, 2);
-		if (IS_ERR(pm->rpivid_asb))
-			pm->rpivid_asb = NULL;
 	} else {
+		struct resource *res;
+
 		pm->base = devm_platform_ioremap_resource_byname(pdev, "pm");
 		if (IS_ERR(pm->base))
 			return PTR_ERR(pm->base);
 
-		pm->asb = devm_platform_ioremap_resource_byname(pdev, "asb");
-		if (IS_ERR(pm->base))
-			pm->asb = NULL;
+		res = platform_get_resource_byname(pdev, IORESOURCE_MEM,
+						    "asb");
+		if (res)
+			pm->asb = devm_ioremap_resource(&pdev->dev, res);
 
-		pm->rpivid_asb = devm_platform_ioremap_resource_byname(pdev,
-								         "rpivid_asb");
-		if (IS_ERR(pm->base))
-			pm->rpivid_asb = NULL;
+		res = platform_get_resource_byname(pdev, IORESOURCE_MEM,
+						    "rpivid_asb");
+		if (res)
+			pm->rpivid_asb = devm_ioremap_resource(&pdev->dev, res);
+	}
 
-		if (pm->rpivid_asb && !is_bcm2711) {
-			dev_err(pm->dev, "RPiVid ASB support only present in BCM2711\n");
-			return -EINVAL;
-		}
+	if (IS_ERR(pm->asb))
+		pm->asb = NULL;
+
+	if (IS_ERR(pm->rpivid_asb))
+		pm->rpivid_asb = NULL;
+
+	if (pm->rpivid_asb && !is_bcm2711) {
+		dev_err(pm->dev, "RPiVid ASB support only present in BCM2711\n");
+		return -EINVAL;
 	}
 
 	return 0;
@@ -108,9 +109,9 @@ static int bcm2835_pm_probe(struct platform_device *pdev)
 }
 
 static const struct of_device_id bcm2835_pm_of_match[] = {
-	{ .compatible = "brcm,bcm2835-pm-wdt", .data = (void *)BCM2835 },
-	{ .compatible = "brcm,bcm2835-pm", .data = (void *)BCM2835 },
-	{ .compatible = "brcm,bcm2711-pm", .data = (void *)BCM2711 },
+	{ .compatible = "brcm,bcm2835-pm-wdt" },
+	{ .compatible = "brcm,bcm2835-pm" },
+	{ .compatible = "brcm,bcm2711-pm" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, bcm2835_pm_of_match);
